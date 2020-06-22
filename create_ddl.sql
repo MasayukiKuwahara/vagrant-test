e13161b2d72ab5e898ca3bae1fb4c115254ff134
MasayukiKuwahara <m-kuwahara@nichiwa-system.co.jp>
2020-06-22 13:12:50 +0900

    Update create_ddl.sql

--- create_ddl.sql
+++ create_ddl.sql
@@ -2,6 +2,6 @@ CREATE DATABASE "RCPT-Web"
   WITH OWNER = postgres
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'ja_JP.UTF-8'
-       LC_CTYPE = 'ja_JP.UTF-8'
+       LC_COLLATE = 'ja_JP.utf8'
+       LC_CTYPE = 'ja_JP.utf8'
        CONNECTION LIMIT = -1;
